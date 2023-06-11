9b3e014c0e7f24b972584d6917efcdd028e2abb2
kumaran-sivapunniyam <kumaran.sivapunniyam@gmail.com>
2023-06-11 07:16:36 -0400

    Biggest of 3 numbers
    Biggest of 3 numbers

---
+++ src/BiggestOfThreeNumbersFinderApp.java
@@ -0,0 +1,57 @@
+
+public class BiggestOfThreeNumbersFinderApp {
+
+	public static void main(String args[]) {
+
+		// Assignment
+		int a = 3000;
+
+		int b = 4000;
+		
+		int c = 5000;
+
+		
+
+		// <, > == : Relational Operators
+
+		// FALSE block
+
+		
+		
+		
+		if (a > b) {
+			
+
+			if( a > c) {
+				
+				System.out.println(" a is the biggest of three numbers.");
+				
+			}
+			else {
+				
+				System.out.println(" c is the biggest of three numbers.");
+				
+			}
+			
+
+		} else {
+
+			if( b > c) {
+				
+				System.out.println(" b is the biggest of three numbers.");
+				
+			}
+			else {
+				
+				System.out.println(" c is the biggest of three numbers.");
+			}
+			
+		}
+		
+			
+		System.out.println("The End");
+		
+	}
+	
+
+}
