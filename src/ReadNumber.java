f3ce34b82c189fac7b3e15f22e3ca2a153b8225a
kumaran-sivapunniyam <kumaran.sivapunniyam@gmail.com>
2023-06-11 14:24:28 -0400

    Reading number from console
    Reading number from console

---
+++ src/ReadNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReadNumber {
+
+	public static void main(String args[]) {
+
+		Scanner scan = new Scanner(System.in);
+
+		// This method reads the number provided using keyboard
+		
+		System.out.print("Enter number1 = ");
+		int number1 = scan.nextInt();
+
+		
+		System.out.print("Enter number2 = ");
+		int number2 = scan.nextInt();
+		
+		int t = number1 + number2; // + Arithmetic Operator
+		System.out.println( "TOTAL = " + t); // + Concatenation Operator
+		
+		
+	}
+
+}
