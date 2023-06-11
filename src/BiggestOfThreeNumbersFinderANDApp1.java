defb5165f525a5f32243d0c427d990914069bd36
kumaran-sivapunniyam <kumaran.sivapunniyam@gmail.com>
2023-06-11 07:49:09 -0400

    Else If demo
    Else If demo

---
+++ src/BiggestOfThreeNumbersFinderANDApp1.java
@@ -0,0 +1,43 @@
+
+public class BiggestOfThreeNumbersFinderANDApp1 {
+
+	public static void main(String args[]) {
+
+		// Assignment
+		int a = 6000;
+
+		int b = 4000;
+
+		int c = 5000;
+
+		// <, > == : Relational Operators
+
+		// FALSE block
+
+		if (a > b && a > c) {
+
+			System.out.println(" a is the biggest of three numbers.");
+
+		}
+		
+		
+		if (b > a && b > c) {
+
+			System.out.println(" b is the biggest of three numbers.");
+
+		}
+		
+		
+		if (c > a && c > b) {
+
+			System.out.println(" c is the biggest of three numbers.");
+
+		}
+
+		
+		
+		System.out.println("The End");
+
+	}
+
+}
---
+++ src/BiggestOfThreeNumbersFinderElseIfApp3.java
@@ -0,0 +1,39 @@
+
+public class BiggestOfThreeNumbersFinderElseIfApp3 {
+
+	public static void main(String args[]) {
+
+		// Assignment
+		int a = 6000;
+
+		int b = 7000;
+
+		int c = 5000;
+
+		// <, > == : Relational Operators
+
+		// FALSE block
+
+		if (a > b && a > c) {
+
+			System.out.println(" a is the biggest of three numbers.");
+
+		}
+		else if (b > a && b > c) {
+
+			System.out.println(" b is the biggest of three numbers.");
+
+		}
+		else if (c > a && c > b) {
+
+			System.out.println(" c is the biggest of three numbers.");
+
+		}
+
+		
+		
+		System.out.println("The End");
+
+	}
+
+}
