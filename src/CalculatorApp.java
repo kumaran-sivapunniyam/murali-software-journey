99e4df400b8834592ec68e0d252896cdbd6b4018
kumaran-sivapunniyam <kumaran.sivapunniyam@gmail.com>
2023-06-21 16:12:17 -0500

    Function Demo
    Function Demo

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -3,6 +3,8 @@ public class CalculatorApp {
 
 	// Method
 	public static void main(String args[]) {
+		
+	
 
 		String name = "Murali";
 		
---
+++ src/HailTheKing.java
@@ -0,0 +1,37 @@
+
+public class HailTheKing {
+
+	public static void hailTheKing(int countOfVazhga, String nameOfTheKing) {
+
+		for (int i = 0; i < countOfVazhga; i = i + 1) {
+			
+			System.out.println("Vazhga...");
+		}
+
+		System.out.println("*******Color Bulb Light Blinking *************");
+		System.out.println(nameOfTheKing);
+		System.out.println("**********************************************");
+		
+		
+		System.out.println("Rajathi Raja Raja Marthaanda Raja Kulothunga 23 aam Pulikesi Varugiraar...");
+
+		System.out.println("........");
+
+	}
+
+	// Name of this method: main
+	public static void main(String[] args) {
+
+		System.out.println("King is in the town...");
+
+		System.out.println("King visiting Big Temple");
+
+		hailTheKing(3, "Ponniyin Selvan");
+
+		System.out.println("King visiting Aranmanai");
+
+		hailTheKing(10, "RajaRaja Chozhan");
+
+	}
+
+}
